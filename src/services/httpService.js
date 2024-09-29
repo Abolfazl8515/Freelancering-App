e6b31deafb7ceb93ce232825d5e330e545debeb4
Abolfazl <boorbooryabolfazl85@gmail.com>
2024-09-29 22:28:00 +0330

    handle refresh token in error added

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,35 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5000/api";
+
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+axiosConfig.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+axiosConfig.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      try {
+        originalConfig._retry = true;
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+        if (data) return axiosConfig(originalConfig);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+      return Promise.reject(err);
+    }
+  }
+);
 
 const http = {
   get: axiosConfig.get,
