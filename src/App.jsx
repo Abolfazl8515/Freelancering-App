66d2cf3c4d5fba209374646e15789600807a763b
Abolfazl <boorbooryabolfazl85@gmail.com>
2024-09-29 21:58:31 +0330

    home page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CompleteProfilePage from "./pages/CompleteProfilePage";
 import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ const App = () => {
         <Toaster />
         <Routes>
           <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="auth" element={<AuthPage />} />
           <Route path="complete-profile" element={<CompleteProfilePage />} />
         </Routes>
